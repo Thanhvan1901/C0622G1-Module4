425c7f3eaeeea28d2915ec8c5a41a18f3b3f8574
thanhvan1901 <you@example.comnvthanh190199@gmail.com>
2022-10-21 18:11:17 +0700

    ss6

---
+++ ss7_springReository/exercise/src/main/java/codegym/repository/IBlogRepository.java
@@ -0,0 +1,13 @@
+package codegym.repository;
+
+import codegym.model.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IBlogRepository extends JpaRepository<Blog,Integer> {
+    /*
+    select * from8 blog where blog_name like %:name%
+     */
+    Page<Blog> findAllByBlogNameContaining(String name,Pageable pageable);
+}
